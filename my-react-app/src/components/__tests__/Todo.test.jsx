2ae16625dd6b66c7001d198d285cda560764fa02
olivier Rukundo <rr.olivier01@gmail.com>
2025-05-02 21:32:51 +0200

    error fixed

--- my-react-app/src/components/__tests__/Todo.test.jsx
+++ my-react-app/src/components/__tests__/Todo.test.jsx
@@ -1,9 +1,20 @@
+import React from 'react';
+import '@testing-library/dom';
 import { render, screen, cleanup } from '@testing-library/react';
 import Todo from '../todo';
 
-test('should render to do component', () => {
-    render(<Todo/>)
+test('should render non-completed to do', () => {
+    const todo =  {id: 1, title: "wash dishes", completed:false };
+    render(<Todo todo={todo}/>)
     const todoElement = screen.getByTestId('todo-1');
     expect(todoElement).toBeInTheDocument();
-    expect(todoElement).toHaveTextContent('Hello world')
-})
+    expect(todoElement).toHaveTextContent('wash dishes')
+});
+
+test('should render completed to do', () => {
+    const todo =  {id: 2, title: "wash car", completed:true };
+    render(<Todo todo={todo}/>)
+    const todoElement = screen.getByTestId('todo-2');
+    expect(todoElement).toBeInTheDocument();
+    expect(todoElement).toHaveTextContent('wash car')
+});
